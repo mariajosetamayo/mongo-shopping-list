4a23722bf692a6d7cca8d194a708a3a2cc3b90af
mjtamayo <mariajose.tamayo@gmail.com>
2016-10-13 17:41:50 +0000

    changed url to database

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://<mariajosetamayo>:<ShihoNague784>@ds023105.mlab.com:23105/shopping-list':
+                            'mongodb://<mariajosetamayo>:<ShihoNague784>@ds023105.mlab.com:23105/shopping-list-dev');
+
+
+
+                            // 'mongodb://localhost/shopping-list' :
+                            // 'mongodb://localhost/shopping-list-dev');
                             
 exports.PORT = process.env.PORT || 8080;
