5e72eb6dd852398e9f92bee3d72c50884f425389
mjtamayo <mariajose.tamayo@gmail.com>
2016-10-13 20:55:05 +0000

    Temp fix of db

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<mariajosetamayo>:<ShihoNague784>@ds023105.mlab.com:23105/shopping-list':
-                            'mongodb://<mariajosetamayo>:<ShihoNague784>@ds023105.mlab.com:23105/shopping-list-dev');
-
-
-
-                            // 'mongodb://localhost/shopping-list' :
-                            // 'mongodb://localhost/shopping-list-dev');
-                            
+                            'mongodb://jack:jack@ds023118.mlab.com:23118/blackjack' : 
+                           // 'mongodb://mariajosetamayo:ShihoNague784@ds023105.mlab.com:23105/shopping-list' :
+                            'mongodb://localhost/shopping-list');
 exports.PORT = process.env.PORT || 8080;
+
+
+                        // 'mongodb://<mariajosetamayo>:<ShihoNague784>@ds023105.mlab.com:23105/shopping-list'
+                     
